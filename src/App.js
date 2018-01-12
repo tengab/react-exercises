4cbd7a88f173c6ebc179b47af2a311a6039d89c3
Tomasz Banach <tomaszbanach01@gmail.com>
2018-01-12 10:26:11 +0100

    juggle between components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import React, {Component} from 'react';
-import MyFirstComponent from './MyFirstComponent'
-import Home from './Home'
-import Dashboard from './Dashboard'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import React from 'react'
 
-class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <MyFirstComponent name='Tomek' showName={true}/>
 
-                    <Route path="/home" component={Home}/>
-                    <Route path="/dashboard" component={Dashboard}/>
-                </div>
-            </Router>
-        );
-    }
-}
+import Concatenation from './Concatenation'
 
-export default App;
+
+const App = () => (
+    <div>
+        <Concatenation
+            firstText="First Text"
+            secondText="Second Text"
+            showSecondText={true}
+        />
+    </div>
+)
+
+export default App
